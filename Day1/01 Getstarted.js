f943a69041a342d424e94feffc516dc3a88f1f9a
Praveenreddy415 <168219404+Praveenreddy415@users.noreply.github.com>
2025-06-26 16:56:33 +0530

    Add files via upload

---
+++ Day1/01 Getstarted.js
@@ -0,0 +1,22 @@
+
+//comment multiple 
+
+/*lines
+use ctrl +a */
+
+
+/*"" -double quotes
+''-single quotes
+`` backticks	*/
+
+//Get started Comment a single line we should use double slash // 
+
+/* Get 
+Started *//*  --> *//* --> Shortcut to comment a paragraph -->Alt+Shift+a */
+
+
+// To declare a string --> "Ravindran", '' , ``
+//Double quotes "", single quotes '', backtick ``
+
+
+
